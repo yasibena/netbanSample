ac8f2f815021c4c0fb378e357cd2edb9cdef98e3
yasibena <118657398+yasibena@users.noreply.github.com>
2024-09-26 20:40:50 +0330

    your commit message

--- src/components/EachAssest.jsx
+++ src/components/EachAssest.jsx
@@ -8,7 +8,7 @@ export default function EachAssest({ each }) {
         
       <div className='bg-[#2a2b3f] grid mt-[0.3rem] rounded-[2px] p-1 grid-cols-8 '>
             <div className='polygon w-[35px] h-[35px] flex items-center justify-center text-[0.6rem]'><span className='text-[#000] font-bold' >{each.grade}</span></div>
-            <div className=''>{each.name}</div>
+            <div className=''>{each.name}name</div>
             <div className='col-span-4  text-center'>{each.total_vuls}</div>
             <div className='col-span-2 text-center'>{moment(each.lastSeen).format('MM/DD/YYYY ')} <span> at</span>{moment(each.lastSeen).format(' HH:MM ')} </div>
       </div>
